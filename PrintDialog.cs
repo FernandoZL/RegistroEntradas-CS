7c08e7d76a7132098cf54f4e106d8c7b6c9b36b9
Fernándo ZL <18036544+FernandoZL@users.noreply.github.com>
2023-08-31 15:01:18 -0600

    pruebas con el numero de turno
    prueba funcional

---
+++ PrintDialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing.Printing;
+
+namespace TurnoApp
+{
+    internal class PrintDialog
+    {
+        public PrinterSettings PrinterSettings { get; internal set; }
+
+        internal object ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using iTextSharp.text.pdf;
 using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -58,7 +61,10 @@ namespace TurnoApp
             string spreadsheetId = "1wqVsSCL0ccH3aGNVd1XWhVOFIUwDgfqLS-toJVWBZ4o";
             string range = "Registros!A:G";
 
-            numeroTurno = CargarNumeroTurno("numeroTurno.txt"); // Cargar el valor del contador desde el archivo
+            numeroTurno = CargarNumeroTurno("numeroTurno.txt");
+            numeroRegistro = CargarNumeroRegistro("numeroRegistro.txt"); // Cargar el valor del contador de registros
+            fechaUltimoRegistro = CargarFechaUltimoRegistro("fechaUltimoRegistro.txt");
+
 
             bool mostrarMenu = true; // Variable para controlar si se muestra el menú principal
 
@@ -75,9 +81,10 @@ namespace TurnoApp
                     {
                         case "1":
                             IngresarNuevoRegistro(registros, spreadsheetId, range, ref numeroTurno);
+                            ImprimirTicket(registros[registros.Count - 1]); // Imprimir el último registro
                             mostrarMenu = false; // Ocultar el menú después de ingresar un nuevo registro
-
                             break;
+
 
 
                         case "2":
@@ -110,10 +117,12 @@ namespace TurnoApp
                 {
                     mostrarMenu = true; // Restablecer el valor para mostrar el menú en el siguiente ciclo
                 }
-                }
             }
+        }
 
-        
+
+
+
         static string MostrarMenu()
         {
             Console.Clear();
@@ -122,7 +131,7 @@ namespace TurnoApp
 
             Console.WriteLine(new string('=', 25));
             Console.WriteLine("\n=== Registro de Unidades  ===\n");
-            
+
             Console.WriteLine("   1. Ingresar nuevo registro");
             Console.WriteLine("   2. Ver registros");
             Console.WriteLine("   3. Salir");
@@ -183,7 +192,7 @@ namespace TurnoApp
 
             Console.WriteLine("\n");
 
-           Console.Write("No. Licencia: ");
+            Console.Write("No. Licencia: ");
             string noLicencia = Console.ReadLine();
 
             Console.Write("Nombre y Apellido: ");
@@ -197,6 +206,11 @@ namespace TurnoApp
 
             DateTime fechaHoraIngreso = DateTime.Now;
 
+            int numeroRegistro = CargarNumeroRegistro("numeroRegistro.txt"); // Cargar el valor del contador de registros
+            numeroRegistro++;
+
+            GuardarNumeroRegistro("numeroRegistro.txt", numeroRegistro);
+
             Registro nuevoRegistro = new Registro(noLicencia, nombreApellido, placas, empresa, fechaHoraIngreso, numeroTurno.ToString());
 
             if (!ExisteRegistro(registros, nuevoRegistro))
@@ -206,6 +220,21 @@ namespace TurnoApp
                 numeroTurno++;
                 GuardarNumeroTurno("numeroTurno.txt", numeroTurno);
 
+
+                // En el método IngresarNuevoRegistro, reinicia el contador si la fecha cambia
+                if (nuevoRegistro.FechaRegistro != DateTime.ParseExact(fechaUltimoRegistro, "yyyy-MM-dd", CultureInfo.InvariantCulture))
+                {
+                    numeroRegistro = 1; // Reiniciar el contador si la fecha cambia
+                    fechaUltimoRegistro = nuevoRegistro.FechaRegistro.ToString("yyyy-MM-dd");
+                    GuardarNumeroRegistro("numeroRegistro.txt", numeroRegistro);
+                }
+                else
+                {
+                    numeroRegistro++; // Incrementar el contador si la fecha es la misma
+                    GuardarNumeroRegistro("numeroRegistro.txt", numeroRegistro);
+                }
+
+
                 AgregarRegistroAGoogleSheets(nuevoRegistro, spreadsheetId, range);
                 AgregarRegistroAExcel(nuevoRegistro, "Registros.xlsx");
 
@@ -213,19 +242,23 @@ namespace TurnoApp
 
                 Console.WriteLine("\n=== Datos Ingresados ===");
                 Console.WriteLine("-------------------------:");
+                Console.WriteLine($"{"Registro :",-25} {nuevoRegistro.Turno}");
+                Console.WriteLine("-------------------------:");
                 Console.WriteLine($"{"No. Licencia:",-25} {nuevoRegistro.NoLicencia}");
                 Console.WriteLine($"{"Nombre y Apellido:",-25} {nuevoRegistro.NombreApellido}");
                 Console.WriteLine($"{"Placas de la Unidad:",-25} {nuevoRegistro.Placas}");
                 Console.WriteLine($"{"Empresa:",-25} {nuevoRegistro.Empresa}");
                 Console.WriteLine($"{"Fecha y Hora de Ingreso:",-25} {nuevoRegistro.FechaHoraIngreso}");
-                Console.WriteLine($"{"No. Turno:",-25} {nuevoRegistro.Turno}");
 
-               
+                Console.WriteLine($"{" ----------------",-25} {FormatearNumeroRegistro(numeroRegistro)}");
+
+
+
 
 
 
                 Console.WriteLine("\nRegistro agregado exitosamente.\n");
-               
+
                 Console.ReadLine();
             }
             else
@@ -233,6 +266,23 @@ namespace TurnoApp
                 Console.WriteLine("\nEl registro ya existe. No se agregó duplicado.");
             }
         }
+
+
+        static void GuardarFechaUltimoRegistro(string filePath, string fecha)
+        {
+            File.WriteAllText(filePath, fecha);
+        }
+
+        static string CargarFechaUltimoRegistro(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllText(filePath);
+            }
+            return DateTime.MinValue.ToString("yyyy-MM-dd"); // Valor predeterminado si no se encuentra el archivo
+        }
+
+
 
         static int CargarNumeroTurno(string filePath)
         {
@@ -246,6 +296,31 @@ namespace TurnoApp
             }
             return 1; // Valor predeterminado si no se encuentra el archivo o no se puede leer el valor
         }
+        static int numeroTurno;
+        static int numeroRegistro;
+        static string fechaUltimoRegistro = DateTime.MinValue.ToString("yyyy-MM-dd");
+
+
+
+        static int CargarNumeroRegistro(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath);
+                if (int.TryParse(content, out int numRegistro))
+                {
+                    return numRegistro; // Devuelve el valor actual sin cambios
+                }
+            }
+            return 1; // Valor predeterminado si no se encuentra el archivo o no se puede leer el valor
+        }
+
+        static void GuardarNumeroRegistro(string filePath, int numRegistro)
+        {
+            File.WriteAllText(filePath, numRegistro.ToString());
+        }
+
+
 
 
         static void GuardarNumeroTurno(string filePath, int numeroTurno)
@@ -263,20 +338,33 @@ namespace TurnoApp
             Console.WriteLine("\n=== Registros ===\n");
 
             Console.WriteLine("\n");
-            Console.WriteLine(new string('=', 120));
-            Console.WriteLine($"| {"No. Licencia",-12} | {"Nombre y Apellido",-20} | {"Placas de la Unidad",-15} | {"Empresa",-12} | {"Fecha y Hora de Ingreso",-25} | {"No. Turno",-12} |");
-            Console.WriteLine(new string('-', 120));
+            Console.WriteLine(new string('=', 130));
+            Console.WriteLine($"| {"No. Registro",-12} | {"No. Licencia",-12} | {"Nombre y Apellido",-20} | {"Placas de la Unidad",-15} | {"Empresa",-12} | {"Fecha y Hora de Ingreso",-25} | {"No. Turno",-12}");
+            Console.WriteLine(new string('-', 130));
 
             foreach (var registro in registros)
             {
-                Console.WriteLine($"| {registro.NoLicencia,-12} | {registro.NombreApellido,-20} | {registro.Placas,-15} | {registro.Empresa,-12} | {registro.FechaHoraIngreso.ToString("dd/MM/yyyy hh:mm tt"),-25} | {FormatearNumeroIngreso(registro.Turno),-12} |");
+                Console.WriteLine($"| {FormatearNumeroIngreso(registro.Turno),-12} | {registro.NoLicencia,-12} | {registro.NombreApellido,-20} | {registro.Placas,-15} | {registro.Empresa,-12} | {registro.FechaHoraIngreso.ToString("dd/MM/yyyy hh:mm tt"),-25} | {FormatearNumeroRegistro(numeroRegistro),-12}");
             }
 
-            Console.WriteLine(new string('=', 120)); // Mostrar línea horizontal al final
+            Console.WriteLine(new string('=', 130)); // Mostrar línea horizontal al final
 
             // Console.WriteLine("Presiona Enter para continuar...");
             // Console.ReadLine();
         }
+
+        private static string FormatearNumeroIngreso(string turno)
+        {
+            return $"0000{turno}"; // Cambia el formato según tus necesidades
+        }
+
+        static string FormatearNumeroRegistro(int numeroRegistro)
+        {
+            return $"Turno: 00{numeroRegistro}";
+        }
+
+
+
         static void GenerarReporte(List<Registro> registros, string excelFilePath)
         {
             Console.WriteLine("Generando reporte PDF...");
@@ -321,21 +409,25 @@ namespace TurnoApp
                 tabla.AddCell(registro.Placas);
                 tabla.AddCell(registro.Empresa);
                 tabla.AddCell(registro.FechaHoraIngreso.ToString("dd/MM/yyyy hh:mm tt"));
-                tabla.AddCell(FormatearNumeroIngreso(registro.Turno));
+                tabla.AddCell(FormatearNumeroIngreso(registro.Turno)); // Reemplaza FormatearNumeroIngreso con FormatearNumeroRegistro
+                tabla.AddCell(FormatearNumeroRegistro(numeroRegistro)); // Reemplaza FormatearNumeroIngreso con FormatearNumeroRegistro
+
+
             }
 
             doc.Add(tabla);
             doc.Close();
 
             Console.WriteLine("Reporte PDF generado exitosamente.");
-            
+
             Console.ReadLine();
         }
 
-        static string FormatearNumeroIngreso(string numeroTurno)
-        {
-            return "Turno: " + numeroTurno.PadLeft(6, '0');
-        }
+
+
+
+
+
 
         static bool ExisteRegistro(List<Registro> registros, Registro nuevoRegistro)
         {
@@ -353,7 +445,9 @@ namespace TurnoApp
         registro.Placas,
         registro.Empresa,
         registro.FechaHoraIngreso.ToString("yyyy-MM-dd HH:mm:ss"),
-        registro.Turno
+        registro.Turno,
+        FormatearNumeroRegistro(numeroRegistro) // Agregar el número de registro aquí
+ 
     });
 
             ValueRange body = new ValueRange
@@ -366,6 +460,53 @@ namespace TurnoApp
             request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
             AppendValuesResponse response = request.Execute();
         }
+
+        static void ImprimirTicket(Registro registro)
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += (sender, e) =>
+            {
+                using (System.Drawing.Font font = new System.Drawing.Font("Arial", 12))
+                using (System.Drawing.Font boldFont = new System.Drawing.Font("Arial", 14, FontStyle.Bold)) // Nuevo estilo de fuente en negrita
+                {
+                    int boxWidth = 280; // Ancho del cuadro
+                    int boxHeight = 220; // Alto del cuadro
+                    int startX = 2; // Posición horizontal desde el borde izquierdo
+                    int startY = 2; // Posición vertical desde el borde superior
+
+                    // Dibujar el cuadro
+                    e.Graphics.DrawRectangle(Pens.Black, startX, startY, boxWidth, boxHeight);
+
+                    int textX = startX + 10; // Margen izquierdo
+                    int textY = startY + 10; // Margen superior
+
+                    e.Graphics.DrawString("=====BIENVENIDO===", font, Brushes.Black, textX, textY); // Sin los dos puntos
+                    textY += 30; // Aumentar el espacio después del título
+
+                    //e.Graphics.DrawString("Número de Registro:", font, Brushes.Black, textX, textY);
+                    //textY += 20;
+                    e.Graphics.DrawString(FormatearNumeroRegistro(numeroRegistro), boldFont, Brushes.Black, textX, textY); // Usar la nueva fuente en negrita
+
+                    textY += 30;
+                    e.Graphics.DrawString($"No. Licencia: {registro.NoLicencia}", font, Brushes.Black, textX, textY);
+                    textY += 20;
+                    e.Graphics.DrawString($"Nombre: {registro.NombreApellido}", font, Brushes.Black, textX, textY);
+                    textY += 20;
+                    e.Graphics.DrawString($"Placas de la Unidad: {registro.Placas}", font, Brushes.Black, textX, textY);
+                    textY += 20;
+                    e.Graphics.DrawString($"Proveedor: {registro.Empresa}", font, Brushes.Black, textX, textY);
+                    textY += 20;
+                    e.Graphics.DrawString($"Fecha: {registro.FechaHoraIngreso.ToString("dd/MM/yyyy HH:mm:ss")}", font, Brushes.Black, textX, textY);
+                    textY += 20;
+                    e.Graphics.DrawString($"No. Registro: {registro.Turno}", font, Brushes.Black, textX, textY);
+                }
+            };
+
+            pd.Print();
+        }
+
+
+
 
 
         static void AgregarRegistroAExcel(Registro registro, string excelFilePath)
@@ -386,6 +527,8 @@ namespace TurnoApp
                     if (!ExisteRegistroEnExcel(registro, worksheet))
                     {
                         worksheet.Cells[startRow, 1].LoadFromCollection(new List<Registro> { registro }, false);
+                        worksheet.Cells[startRow, 7].Value = FormatearNumeroRegistro(numeroRegistro); // Nueva columna
+
                     }
                 }
 
@@ -402,23 +545,25 @@ namespace TurnoApp
 
 
 class Registro
-        {
-            public string NoLicencia { get; set; }
-            public string NombreApellido { get; set; }
-            public string Placas { get; set; }
-            public string Empresa { get; set; }
-            public DateTime FechaHoraIngreso { get; set; }
-            public string Turno { get; set; }
+{
+    public string NoLicencia { get; set; }
+    public string NombreApellido { get; set; }
+    public string Placas { get; set; }
+    public string Empresa { get; set; }
+    public DateTime FechaHoraIngreso { get; set; }
+    public string Turno { get; set; }
+    public DateTime FechaRegistro { get; set; }
 
-            public Registro(string noLicencia, string nombreApellido, string placas, string empresa, DateTime fechaHoraIngreso, string turno)
-            {
-                NoLicencia = noLicencia;
-                NombreApellido = nombreApellido;
-                Placas = placas;
-                Empresa = empresa;
-                FechaHoraIngreso = fechaHoraIngreso;
-                Turno = turno;
-            }
-        }
-    
+    public Registro(string noLicencia, string nombreApellido, string placas, string empresa, DateTime fechaHoraIngreso, string turno)
+    {
+        NoLicencia = noLicencia;
+        NombreApellido = nombreApellido;
+        Placas = placas;
+        Empresa = empresa;
+        FechaHoraIngreso = fechaHoraIngreso;
+        Turno = turno;
+        FechaRegistro = fechaHoraIngreso.Date; // Almacenar solo la fecha sin la hora
+    }
+}
+
 
